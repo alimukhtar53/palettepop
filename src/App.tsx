b4aae3e546f509aaa271fc37f14b5bd8bbc9a44b
alimukhtar53 <alimukhtar52@gmail.com>
2023-04-29 04:10:28 +0500

    Displayed color palette inside grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import { Container, Card, Row, Text } from "@nextui-org/react";
+import { Grid, Card, Text, Container } from "@nextui-org/react";
+import { useState } from "react";
+
+interface Props {
+  text: string;
+  color?: string;
+}
 
 export default function App() {
+  const items = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+  const MockItem = ({ text, color }: Props) => {
+    return (
+      <div>
+        <Card css={{ h: "$20", w: "$36", $$cardColor: color }}>
+          <Card.Body></Card.Body>
+        </Card>
+        <Text h6 size={15} color="black" css={{ m: "auto" }}>
+          {text}
+        </Text>
+      </div>
+    );
+  };
   return (
-    <Container sm>
-      <Card css={{ $$cardColor: "$colors$primary" }}>
-        <Card.Body>
-          <Row justify="center" align="center">
-            <Text h6 size={15} color="white" css={{ m: 0 }}>
-              NextUI gives you the best developer experience with all the
-              features you need for building beautiful and modern websites and
-              applications.
-            </Text>
-          </Row>
-        </Card.Body>
-      </Card>
+    <Container>
+      <Grid.Container gap={2} justify="center">
+        {items.map((item) => (
+          <Grid lg key={item}>
+            <MockItem text={`${item}`} color="#eab308" />
+          </Grid>
+        ))}
+      </Grid.Container>
     </Container>
   );
 }
